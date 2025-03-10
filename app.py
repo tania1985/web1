470942293652d0be75fc64f32f44c8d96f42ee70
tania1985 <testevezbautista@danielcastelao.org>
2025-03-10 17:17:07 +0100

    mejorando el python

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,12 +6,21 @@ app = Flask(__name__)
 def index ():
     return render_template ('index.html')
 
+#Recibir dos parametros
 @app.route('/saludo/<nombre>/<int:edad>')
 def saludo(nombre,edad):
     if(edad<18):
         return 'Hola ' + nombre + "eres mayor de edad"
     else:
         return 'Hola ' + nombre + "eres menor de edad"
+
+#Login post
+@app.route('/login', methods=['POST'])
+def login():
+    #Obtener datos del formulario
+    username = request.form['username']
+    password = request.form['password']
+    return f"Nombre: {username} Pass: {password}"   
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
